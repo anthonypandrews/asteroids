be8d354b8be6adafda9de41413e38bd5f3f5bc45
anthonypandrews <anthonypandrews95@gmail.com>
2026-01-27 22:36:57 -0500

    Added clock object and reduced FPS compute

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Clock object
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Game loop
     while True:
         log_state()
+        
+        # Add 'x out' functionality to game window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
+
+        # Cap FPS at 60 and compute delta time (in seconds)
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
