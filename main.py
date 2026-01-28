a16d153e5ca0fdfea1637d92fa0897f362b14f89
anthonypandrews <anthonypandrews95@gmail.com>
2026-01-28 15:00:02 -0500

    created Player class

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
+    # pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+    
+    # Player setup
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    # Welcome message
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # Clock object
-    clock = pygame.time.Clock()
-    dt = 0
-
     # Game loop
-    while True:
+    while running:
         log_state()
         
-        # Add 'x out' functionality to game window
+        # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
         
+        # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
+        
+        # draw the player
+        player.draw(screen)
+
+        # flip() the display to put your work on screen
         pygame.display.flip()
 
         # Cap FPS at 60 and compute delta time (in seconds)
